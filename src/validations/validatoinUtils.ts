1f4ed0e4fab8034ea79049026d2fb13d804923e6
BROCHSTEIN RAZ <RAZBRO@rf.local>
2023-07-27 17:52:46 +0300

    with validationUtils

---
+++ src/validations/validatoinUtils.ts
@@ -0,0 +1,7 @@
+const isValidCoords = (coordinates: any) => {
+    if (Array.isArray(coordinates)) {
+        if (coordinates.length === 1){
+            if (typeof coordinates[0] === "Array")
+        }
+    }
+}
